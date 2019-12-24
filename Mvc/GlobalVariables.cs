6d88cce0b516ae3fac4f4801ff29e7d0c18b4a71
tahidularafhin <48841466+tahidularafhin@users.noreply.github.com>
2019-12-24 16:24:22 +0100

    first commit

---
+++ Mvc/GlobalVariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+
+namespace Mvc
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+      static  GlobalVariables()
+        {
+            WebApiClient.BaseAddress = new Uri("http://localhost:50712/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ Mvc/Models/mvcEmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Models
+{
+    public class mvcEmployeeModel
+    {
+
+        public int EmployeeID { get; set; }
+        [Required(ErrorMessage ="This Field is Required")]
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
